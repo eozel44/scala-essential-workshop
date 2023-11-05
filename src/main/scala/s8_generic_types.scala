280160333c0f170fa61a4bf8a8a961acf9189e1f
eren <eozel44@gmail.com>
2023-11-05 13:42:31 +0300

    code organization

--- src/main/scala/s8_generic_types.scala
+++ src/main/scala/s8_generic_types.scala
@@ -1,5 +1,63 @@
 object s8_generic_types {
 
+  /***
+   * Generic Algebraic Data Types
+   *
+   * --Invariant Generic Sum Type Pattern
+   *
+   * If A of type T is a B or C write
+
+          sealed trait A[T]
+          final case class B[T]() extends A[T]
+          final case class C[T]() extends A[T]
+   *
+   */
+  sealed trait Result[A]
+  case class Success[A](result: A) extends Result[A]
+  case class Failure[A](reason: String) extends Result[A]
+
+  sealed trait LinkedList[A] {
+    def length: Int =
+      this match {
+        case Pair(hd, tl) => 1 + tl.length
+        case End() => 0
+      }
+
+    def contains(item: A): Boolean =
+      this match {
+        case Pair(hd, tl) =>
+          if(hd == item)
+            true
+          else
+            tl.contains(item)
+        case End() => false
+      }
+
+    def get(index: Int): A =
+      this match {
+        case Pair(hd, tl) =>
+          if(index == 0)
+            hd
+          else
+            tl.get(index - 1)
+        case End() =>
+          throw new Exception("Index out of bounds")
+      }
+
+    def apply(index: Int): Result[A] =
+      this match {
+        case Pair(hd, tl) =>
+          if(index == 0)
+            Success(hd)
+          else
+            tl(index - 1)
+        case End() =>
+          Failure("Index out of bounds")
+      }
+  }
+  final case class Pair[A](head: A, tail: LinkedList[A]) extends LinkedList[A]
+  final case class End[A]() extends LinkedList[A]
+
   def main(args: Array[String]): Unit = {
     /***
      * Generics
@@ -19,7 +77,7 @@ object s8_generic_types {
     boxMethod(1)
 
 
-    //check linkedlist definition below
+    //check linkedlist definition above
 
     val example = Pair(1, Pair(2, Pair(3, End())))
     val exampleStr = Pair("eren", Pair("hello", Pair("scala", End())))
@@ -50,66 +108,5 @@ object s8_generic_types {
     assert(example(3) == Failure("Index out of bounds"))
 
   }
-
-  /***
-   * Generic Algebraic Data Types
-   *
-   * --Invariant Generic Sum Type Pattern
-   *
-   * If A of type T is a B or C write
-
-          sealed trait A[T]
-          final case class B[T]() extends A[T]
-          final case class C[T]() extends A[T]
-   *
-   */
-      sealed trait Result[A]
-      case class Success[A](result: A) extends Result[A]
-      case class Failure[A](reason: String) extends Result[A]
-
-  sealed trait LinkedList[A] {
-    def length: Int =
-      this match {
-        case Pair(hd, tl) => 1 + tl.length
-        case End() => 0
-      }
-
-    def contains(item: A): Boolean =
-      this match {
-        case Pair(hd, tl) =>
-          if(hd == item)
-            true
-          else
-            tl.contains(item)
-        case End() => false
-      }
-
-    def get(index: Int): A =
-      this match {
-        case Pair(hd, tl) =>
-          if(index == 0)
-            hd
-          else
-            tl.get(index - 1)
-        case End() =>
-          throw new Exception("Index out of bounds")
-      }
-
-          def apply(index: Int): Result[A] =
-            this match {
-              case Pair(hd, tl) =>
-                if(index == 0)
-                  Success(hd)
-                else
-                  tl(index - 1)
-              case End() =>
-                Failure("Index out of bounds")
-            }
-  }
-  final case class Pair[A](head: A, tail: LinkedList[A]) extends LinkedList[A]
-  final case class End[A]() extends LinkedList[A]
-
-
-
 
 }
